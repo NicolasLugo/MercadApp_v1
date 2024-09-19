2c9a809dbc7dca10c5a0d84a3397bb312376202f
NicolasLugo <nilupa720@gmail.com>
2024-09-18 23:31:51 -0300

    se agregan funcionalidades a los botones para que haya interactividad

--- app/src/main/java/com/example/mercadapp_v1/InicioActivity.java
+++ app/src/main/java/com/example/mercadapp_v1/InicioActivity.java
@@ -1,24 +1,39 @@
 package com.example.mercadapp_v1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class InicioActivity extends AppCompatActivity {
+private Button btnInformes;
+private Button btnHistorico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_inicio);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnInformes = findViewById(R.id.btnInformes);
+        btnHistorico = findViewById(R.id.btnHistorico);
+
+        btnInformes.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                mostrarInformes();
+            }
         });
+
+        /* esta función la usaría en caso tal de tener diferentes tipos de usuario y poder
+        darles una bienvenida más única, para eso, tendría que tener un textview en blanco en el que pueda
+        colocar el mensaje de bienvenida
+        Intent intent = getIntent();
+        String nombre = intent.getExtras().getString("nombre");
+        -TextView-.setText("Bienvenido, " + nombre);
+        */
+    }
+
+    public void mostrarInformes(){
     }
 }
--- app/src/main/java/com/example/mercadapp_v1/LoginActivity.java
+++ app/src/main/java/com/example/mercadapp_v1/LoginActivity.java
@@ -1,24 +1,74 @@
 package com.example.mercadapp_v1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView btnNewUser;
+    private EditText txtUsuario;
+    private EditText txtContrasena;
+    private Button btnIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        txtUsuario = findViewById(R.id.txtUsuario);
+        txtContrasena = findViewById(R.id.txtContrasena);
+        btnIngresar = findViewById(R.id.btnIngresar);
+        btnNewUser = findViewById(R.id.btnNewUser);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                validarLogin();
+            }
         });
+        btnNewUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nuevoUsuario();
+            }
+        });
+    }
+
+    public void validarLogin(){
+        //Se obtienen los valores ingresados por el usuario
+        String user = txtUsuario.getText().toString();
+        String passw = txtContrasena.getText().toString();
+
+        //Registro de inicio de sesión de prueba
+        String validUser = "admin";
+        String validPassw = "1234";
+
+        //Se comparan ambos valores
+        if(user.equals(validUser) && passw.equals(validPassw)){
+            //Se inicia la nueva actividad si los valores son iguales
+            Intent intent = new Intent(this, InicioActivity.class);
+
+            /*usaría esta línea cuando tuviera varios usuarios y así tener un inicio de sesión
+            para cada uno, usando los datos de ingreso para darles la bienvenida
+            intent.putExtra("nombre", user);
+            */
+
+            startActivity(intent);
+        }else{
+            //Se muestra un mensaje de error
+            Toast.makeText(this,"Datos incorrectos, intente nuevamente", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    public void nuevoUsuario(){
+        Intent intent = new Intent(this, NewUserActivity.class);
+        startActivity(intent);
     }
 }
